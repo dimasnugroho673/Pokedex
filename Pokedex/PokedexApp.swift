243a8cd9418edbd70d778bfe7c5aafca0d9b1c34
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2022-05-25 11:31:45 +0700

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Dimas Putro on 25/05/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
